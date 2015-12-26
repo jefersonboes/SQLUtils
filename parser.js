12a33e6e4116f90891bfbd00eb6ad979fcfd737a
Jeferson <jefersonboes@gmail.com>
2015-12-26 14:28:07 -0200

    .

--- parser.js
+++ parser.js
@@ -1,22 +1,47 @@
 
+function quote_str(text) {
+	var result = "";
+
+	var i = 0;
+
+	while (i < text.length) {		
+		if (text[i] == "'") {
+			var qs = "";
+			while (text[i] == "'") {
+				qs += text[i];
+				i++;
+			}
+
+			result += "'" + qs + "'";
+
+			continue;
+		} else 
+			result += text[i];
+
+		i++;
+	}
+
+	return result;
+}
+
 function concat_sql(text) {
 
-	split_text = text.split("\x0a");
+	var split_text = text.split("\x0a");
 
-	new_text = "";
+	var new_text = "";
 
-	for (i = 0;  i < split_text.length; i++) {
-		s = trim(split_text[i]);
+	for (var i = 0;  i < split_text.length; i++) {
+		var s = trim(split_text[i]);
 
-		ns = "'" + s + "'";
+		s = quote_str(s);
+
+		var ns = "'" + s + "'";
 
 		if (i < split_text.length - 1)
 			ns = ns + "#13+";
 
 		new_text += ns;
 		new_text += '\x0a';
-
-		console.log(ns);
 	}
 
 	return new_text;
