adc48f14301fa20e57ecba92e445043f3aa359cd
Jeferson <jefersonboes@gmail.com>
2015-12-26 20:39:45 -0200

    .

--- page.js
+++ page.js
@@ -1,10 +1,35 @@
+/* SQL Utils - SQL Utils
+ * Copyright (C) 2015 Jeferson Boes
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 window.onload = function() {
 	var data = document.getElementById("data");
-	var butGo = document.getElementById("butGo");
+	var butQuote = document.getElementById("butQuote");
+	var butExtract = document.getElementById("butExtract");
 	var result = document.getElementById("result");
 		
-	butGo.onclick = function(e) {				    
+	butQuote.onclick = function(e) {				    
 	    result.value = concat_sql(data.value);
+	    e.preventDefault();
+	}
+
+	butExtract.onclick = function(e) {				    
+	    result.value = extract_sql(data.value);
 	    e.preventDefault();
 	}
 }
--- parser.js
+++ parser.js
@@ -1,3 +1,21 @@
+/* SQL Utils - SQL Utils
+ * Copyright (C) 2015 Jeferson Boes
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 function quote_str(text) {
 	var result = "";
@@ -49,4 +67,56 @@ function concat_sql(text) {
 
 function trim(str) {
 	return str.replace(/^\s+|\s+$/g,"");
+}
+
+function extract_begin_line(text) {
+	var i = 0;
+	var line = "";
+
+	while (i < text.length) {		
+		if (text[i] == "'") {
+			
+			while (text[i] == "'") {
+				i++;
+			}
+
+			while (i < text.length) {
+				line += text[i];
+				i++;
+			}
+		} else
+			i++;
+	}
+
+	return line;
+}
+
+function extract_end_line(text) {
+	var i = text.length;
+
+	while (i > 0) {
+		if (text[i] == "'")
+			break;
+
+		i--;
+	}
+
+	console.log(text);
+
+	return text.substr(0, i);
+}
+
+function extract_sql(text) {
+	var split_text = text.split("\x0a");
+	var result = "";
+
+	for (var i = 0;  i < split_text.length; i++) {
+		if (result.length > 0)
+			result += '\x0a';
+
+		var part = extract_begin_line(split_text[i]);
+		result += extract_end_line(part);		
+	}
+
+	return result;
 }
